74d5bb14d3f2278f14ce3f3a77aee26bb696a362
shahariaryan <shahariarahmed834@gmail.com>
2021-04-12 22:48:23 +0600

    database

---
+++ db_GUI/db_GUI/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace db_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nametb = new System.Windows.Forms.TextBox();
+            this.usernametb = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.Label();
+            this.passwordtb = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.Label();
+            this.emailtb = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.Label();
+            this.add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nametb
+            // 
+            this.nametb.Location = new System.Drawing.Point(138, 33);
+            this.nametb.Name = "nametb";
+            this.nametb.Size = new System.Drawing.Size(244, 40);
+            this.nametb.TabIndex = 1;
+            // 
+            // usernametb
+            // 
+            this.usernametb.Location = new System.Drawing.Point(156, 89);
+            this.usernametb.Name = "usernametb";
+            this.usernametb.Size = new System.Drawing.Size(226, 40);
+            this.usernametb.TabIndex = 3;
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Location = new System.Drawing.Point(27, 96);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(131, 33);
+            this.username.TabIndex = 2;
+            this.username.Text = "Username";
+            // 
+            // passwordtb
+            // 
+            this.passwordtb.Location = new System.Drawing.Point(156, 209);
+            this.passwordtb.Name = "passwordtb";
+            this.passwordtb.Size = new System.Drawing.Size(226, 40);
+            this.passwordtb.TabIndex = 5;
+            this.passwordtb.UseSystemPasswordChar = true;
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(34, 212);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(124, 33);
+            this.password.TabIndex = 4;
+            this.password.Text = "Password";
+            this.password.Click += new System.EventHandler(this.password_Click);
+            // 
+            // emailtb
+            // 
+            this.emailtb.Location = new System.Drawing.Point(156, 152);
+            this.emailtb.Name = "emailtb";
+            this.emailtb.Size = new System.Drawing.Size(226, 40);
+            this.emailtb.TabIndex = 7;
+            // 
+            // email
+            // 
+            this.email.AutoSize = true;
+            this.email.Location = new System.Drawing.Point(75, 155);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(75, 33);
+            this.email.TabIndex = 6;
+            this.email.Text = "Email";
+            this.email.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // add
+            // 
+            this.add.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.add.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.add.Location = new System.Drawing.Point(138, 255);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(109, 51);
+            this.add.TabIndex = 8;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = false;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(412, 367);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.emailtb);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.passwordtb);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.usernametb);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.nametb);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Candara Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nametb;
+        private System.Windows.Forms.TextBox usernametb;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.TextBox passwordtb;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.TextBox emailtb;
+        private System.Windows.Forms.Label email;
+        private System.Windows.Forms.Button add;
+    }
+}
---
+++ db_GUI/db_GUI/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace db_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void password_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            string name = nametb.Text;
+            string username = usernametb.Text;
+            string email = emailtb.Text;
+            string password = passwordtb.Text;
+
+            if (name.Trim().Length==0 || username.Trim().Length == 0 || password.Trim().Length == 0 || email.Trim().Length == 0)
+            {
+                MessageBox.Show("Enter all required field");
+            }
+            else
+            {
+
+                string connectionString = @"Server=DESKTOP-F2QN1PO\SQLEXPRESS;Database=demo;User Id=sa;Password=165757;";
+                SqlConnection conn = new SqlConnection(connectionString);
+                conn.Open();
+
+                string query = string.Format("INSERT INTO Table_1 VALUES ('{0}', '{1}', '{2}', '{3}')", name, username, password, email);
+                SqlCommand cmd = new SqlCommand(query, conn);
+                var r = cmd.ExecuteNonQuery();
+                if (r > 0)
+                {
+                    MessageBox.Show("Inserted");
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
